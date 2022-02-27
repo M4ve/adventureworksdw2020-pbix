866755b168e5559506dca822eefdafd909e47b05
mave <geraldberndt@gmail.com>
2022-02-27 15:03:16 +0100

    Changed Postal Code -> zip & added Report for Testing

--- pbix/Model/queries/Customer.m
+++ pbix/Model/queries/Customer.m
@@ -1,6 +1,7 @@
 let
     Source = Csv.Document(Web.Contents(HttpSource & "Customer.csv"),[Delimiter=",", Columns=7, Encoding=65001, QuoteStyle=QuoteStyle.None]),
     #"Promoted Headers" = Table.PromoteHeaders(Source, [PromoteAllScalars=true]),
-    #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"CustomerKey", Int64.Type}, {"Customer ID", type text}, {"Customer", type text}, {"City", type text}, {"State-Province", type text}, {"Country-Region", type text}, {"Postal Code", type text}})
+    #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"CustomerKey", Int64.Type}, {"Customer ID", type text}, {"Customer", type text}, {"City", type text}, {"State-Province", type text}, {"Country-Region", type text}, {"Postal Code", type text}}),
+    #"Umbenannte Spalten" = Table.RenameColumns(#"Changed Type",{{"Postal Code", "ZIP"}})
 in
-    #"Changed Type"
+    #"Umbenannte Spalten"
